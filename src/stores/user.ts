0dae8fc5a543ec57ade9fffd85a336255b96fb95
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-05-30 09:04:31 -0400

    chore: Refactor user store and login view

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,18 +1,44 @@
-import { ref, computed } from 'vue'
+// import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 interface User {
-    id:       number;
-    name:     string;
-    lastname: string;
-    password: string;
+  id:            number;
+  name:          string;
+  lastname:      string;
+  password:      string;
+  authenticated: boolean;
 }
 
 export const useCounterStore = defineStore('user', () => {
-   
-    const login = () => {
+  let isAuthenticated:   boolean = false;
+  let isLoading:         boolean = false;
+  const Users: User[] = [
+    { id: 1, name: 'John', lastname: 'Doe', password: '1234', authenticated: false },
+    { id: 2, name: 'Jane', lastname: 'Doe', password: '1234', authenticated: false },
+  ]
 
+  const login = ( name: string, password: string ): User => {
+    isLoading = true;
+    for (const user of Users) {
+      if (user.name === name && user.password === password) {
+        user.authenticated = true
+        isAuthenticated = true;
+        isLoading = false;
+        return user
+      }
     }
+    isAuthenticated = false;
+    isLoading       = false;
+    throw new Error('User not found');
+  }
 
-  return { login,  }
+  const logout = ( id: number ): void => {
+    const user = Users.find(user => user.id === id)
+    if (user) {
+      user.authenticated = false
+    }
+    isAuthenticated = false;
+  }
+
+  return { login, isAuthenticated, isLoading, logout, Users }
 })
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 
 </script>
-
-<template>
     
+<template>
+    <main>
+        <h1>Login Page</h1>
+        <h3>Por favor ingresa tus credenciales personales</h3>
+    </main>    
 </template>
+
+<style>
+
+</style>
