a99c9195b2762e8760fa8b9871cd71758b112f02
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-05-30 21:01:43 -0400

    chore: Update vite.config.ts with base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: 'https://cedricnator.github.io/vue_taller-3',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
