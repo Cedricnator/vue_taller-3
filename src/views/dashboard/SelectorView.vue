1bdceaf00f44c23bed129ebff5cbaf9ff38218c9
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-02 20:35:39 -0400

    chore: Update SelectorView component to improve code formatting and readability

--- src/views/dashboard/SelectorView.vue
+++ src/views/dashboard/SelectorView.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
-import CharacterSelector from '@/components/CharacterSelector.vue'
+import CharacterSelector      from '@/components/CharacterSelector.vue'
 import CardCharacterSelection from '@/components/CardCharacterSelection.vue';
-import CardMapSelection from '@/components/CardMapSelection.vue';
+import CardMapSelection       from '@/components/CardMapSelection.vue';
 </script>
 
 <template>
-   <div class="flex flex-col justify-around">
-      <div class="flex flex-row justify-between items-center px-16">
-
-         <CardCharacterSelection nameCharacter="Goku" imgCharacter="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" />
+   <main class="flex flex-col justify-around">
+      <section class="flex flex-row justify-between items-center px-16">
+         <CardCharacterSelection 
+            nameCharacter="Goku" 
+            imgCharacter="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" 
+         />
 
          <CardMapSelection />
 
-         <CardCharacterSelection nameCharacter="Goku" imgCharacter="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" />
-      </div>
-      <div>
+         <CardCharacterSelection 
+            nameCharacter="Goku" 
+            imgCharacter="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+         />
+      </section>
+      <section>
          <CharacterSelector />
-      </div>
-   </div>
+      </section>
+   </main>
 </template>
 
 <style scoped></style>
